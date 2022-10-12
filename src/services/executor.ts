b0fe747db662d8a6a44e5bd45773cfbb8646a531
Juraj Bačovčin <hehe100596@yahoo.com>
2022-10-12 13:47:23 +0200

    minor refactoring and implementing simple retry mechanism for failed transactions

--- src/services/executor.ts
+++ src/services/executor.ts
@@ -9,29 +9,31 @@ import { updateTask } from '../clients/dynamo';
 
 module.exports.main = async (event: SNSEvent): Promise<Response> => {
   let error: Error;
+  let tries = 3;
 
   for (const record of event.Records) {
     const amm = JSON.parse(record.Sns?.Message)?.ammId;
 
     if (amm) {
-      await executeFundingPayment(amm)
-        .then(async (res) => {
-          if (res.status === 1) {
-            console.log(`${amm}: funding payment successful - ${res.txHash}`);
+      while (tries > 0) {
+        await executeFundingPayment(amm)
+          .then(async (res) => {
+            if (res.status === 1) {
+              console.log(`${amm}: funding payment successful - ${res.txHash}`);
 
-            await updateTask(amm, getTimestampInSeconds()).catch((e) => {
-              error = generateError(
-                `failed to add new task for ${amm.id} to DB`,
-                JSON.stringify(e),
-              );
-            });
-          } else {
-            error = generateError(`${amm}: funding payment unsuccessful - ${res.txHash}`);
-          }
-        })
-        .catch((e) => {
-          error = generateError(`${amm}: failed to execute funding payment`, JSON.stringify(e));
-        });
+              await updateTask(amm, getTimestampInSeconds()).catch((e) => {
+                error = generateError(`failed to update task ${amm} in DB`, JSON.stringify(e));
+              });
+            } else {
+              error = generateError(`${amm}: funding payment unsuccessful - ${res.txHash}`);
+            }
+            tries = 0;
+          })
+          .catch((e) => {
+            error = generateError(`${amm}: failed to execute funding payment`, JSON.stringify(e));
+            tries--;
+          });
+      }
     } else {
       error = generateError('invalid sns message received');
     }
